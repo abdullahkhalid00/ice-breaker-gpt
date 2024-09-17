93dd692857ed81675b3f9f800dd82aa8d69b6d9d
Abdullah Khalid <iabdulahk@gmail.com>
2024-09-17 07:12:13 +0500

    Create a function to generate question

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import os
-
 from groq import Groq
-from utils import load_instructions
 
 
 client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
 
-system_prompt = {
-    'role': 'system',
-    'content': load_instructions('./instructions.txt')
-}
-
-response = client.chat.completions.create(
-    model='gemma2-9b-it',
-    messages=[system_prompt],
-    max_tokens=1024,
-    temperature=1.2,
-    stream=None,
-    stop=None,
-)
-
-print(response.choices[0].message.content.strip())
+def generate_question(client, instructions):
+    response = client.chat.completions.create(
+        model='gemma2-9b-it',
+        messages=[
+            {
+                'role': 'system',
+                'content': instructions
+            }
+        ],
+        max_tokens=1024,
+        temperature=1.2,
+        stream=None,
+        stop=None,
+    )
+    return response.choices[0].message.content.strip()
