6c098f5ac7578802068b2933f2acb9ff90dedec7
Abdullah Khalid <iabdulahk@gmail.com>
2024-09-18 02:11:23 +0500

    Merge Groq client

--- inference.py
+++ inference.py
@@ -1,10 +1,10 @@
 import os
 from groq import Groq
+from utils import load_instructions
 
 
-client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
-
-def generate_question(client, instructions):
+def generate_question(instructions=load_instructions('./instructions.txt')):
+    client = Groq(api_key=os.environ.get('GROQ_API_KEY'))
     response = client.chat.completions.create(
         model='gemma2-9b-it',
         messages=[
