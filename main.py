60a63fdd4a8ed37d3a9bad78f02ca37cd1161539
Abdullah Khalid <iabdulahk@gmail.com>
2024-09-18 02:11:39 +0500

    Add Gradio UI

---
+++ main.py
@@ -0,0 +1,19 @@
+import gradio as gr
+from inference import generate_question
+
+
+def main():
+    app = gr.Interface(
+        title='Ice Breaker GPT 🧊🔨',
+        fn=generate_question,
+        inputs=[],
+        outputs=gr.Textbox(label='Host'),
+        theme=gr.themes.Monochrome(
+            font=[gr.themes.GoogleFont('Inconsolata'), 'Arial', 'sans-serif']
+        )
+    )
+    app.launch()
+
+
+if __name__ == '__main__':
+    main()
