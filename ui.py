af478a8498936648964d9855a2b5499730a2fd3a
Abdullah Khalid <iabdulahk@gmail.com>
2024-09-18 02:35:27 +0500

    Update ui.py

--- ui.py
+++ ui.py
@@ -2,19 +2,13 @@ import gradio as gr
 from inference import generate_question
 
 
-def main():
-    app = gr.Interface(
-        title='Ice Breaker GPT 🧊🔨',
-        fn=generate_question,
-        inputs=[],
-        outputs=gr.Textbox(label='Host'),
-        theme=gr.themes.Monochrome(
-            font=[gr.themes.GoogleFont('Inconsolata'), 'Arial', 'sans-serif']
-        ),
-        allow_flagging='never',
-    )
-    app.launch()
-
-
-if __name__ == '__main__':
-    main()
+demo = gr.Interface(
+    title='Ice Breaker GPT 🧊🔨',
+    fn=generate_question,
+    inputs=[],
+    outputs=gr.Textbox(label='Host'),
+    theme=gr.themes.Monochrome(
+        font=[gr.themes.GoogleFont('Inconsolata'), 'Arial', 'sans-serif']
+    ),
+    allow_flagging='never',
+)
